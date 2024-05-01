fac857f7940ad20275961fe525a2d13c2e5e4c69
GuvidSlayer <andgeorge0015@gmail.com>
2024-05-01 23:44:34 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,22 +1,23 @@
-const startBtn = document.querySelector('.js-start');
-const stopBtn = document.querySelector('.js-stop');
-let timerId = null;
-
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, '0')}`;
 }
 
-startBtn.addEventListener('click', () => {
-  startBtn.disabled = true;
-  stopBtn.disabled = false;
-  timerId = setInterval(() => {
+let intervalId;
+
+document.getElementById('startButton').addEventListener('click', function () {
+  this.disabled = true;
+  document.getElementById('stopButton').disabled = false;
+
+  intervalId = setInterval(function () {
     document.body.style.backgroundColor = getRandomHexColor();
   }, 1000);
 });
 
-stopBtn.addEventListener('click', () => {
-  clearInterval(timerId);
-  console.log(`Interval with id ${timerId} has stopped!`);
-  startBtn.disabled = false;
-  stopBtn.disabled = true;
+document.getElementById('stopButton').addEventListener('click', function () {
+  this.disabled = true;
+  document.getElementById('startButton').disabled = false;
+
+  clearInterval(intervalId);
 });
